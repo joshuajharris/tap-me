835c5760b2aebfc8648a35ec04a76f84bcd0772b
Joshua Harris <joshua.jharris13@gmail.com>
2015-10-29 22:41:03 -0400

    finished tut

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    var count = 0
+    var seconds = 0
+    var timer = NSTimer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupGame()
     }
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        count++
+        scoreLabel.text = "Score: \(count)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func setupGame()  {
+        seconds = 30
+        count = 0
+        
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+    }
+    
+    func subtractTime() {
+        seconds--
+        timerLabel.text = "Time: \(seconds)"
+        
+        if(seconds == 0){
+            timer.invalidate()
+            let alert = UIAlertController(title: "Time is up!", message: "You scored \(count) points", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertActionStyle.Default, handler: {
+                action in self.setupGame()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
 
 }
 
